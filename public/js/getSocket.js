e4bca49712a71e6ef659555975ef1708e7811f66
Allen Heavey <ista.allen@gmail.com>
2013-05-25 14:37:03 +0800

    emulator setInterval

--- public/js/getSocket.js
+++ public/js/getSocket.js
@@ -1,11 +1,18 @@
 
 (function(){
 
-    var socket = io.connect('http://dmdgeeker.com:7887');
+    var socket = io.connect('http://dmdgeeker.com:7887')
+      , times = 0;
 
     socket.on('id', function (index) {
         console.log(index);
         socket.emit('update', "Hello, this is web. " + index);
+
+        setInterval(function(){
+            times++;
+            socket.emit('update', "ID: " + index + " has invoked " + times + " times.");
+        }, 2000);
+
         socket.on('callback', function(cb){
             console.log(cb);
         });
